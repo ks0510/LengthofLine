d1778466722e3de336943fcd85e48755c6061db2
ks0510 <kshaikh051999@gmail.com>
2023-04-07 15:45:35 +0530

    Added length of line java program

---
+++ LengthofLine.java
@@ -0,0 +1,26 @@
+package practice;
+import java.util.*;
+
+/**
+ * 
+ * @author Kaif
+ *
+ */
+
+public class GeometryLine {
+	/**
+	 * This method is created for calculating the Length of lines using 
+	 * cartesian system,comparing length of lines
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 
+		 */	
+		System.out.println("Welcome to Line Comparison Computation Program");
+		
+		         
+     }
+}
